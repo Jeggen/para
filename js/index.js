93f68f42050e58435b133c8054066aa88f7c34bf
Jake <j.eggen@cevesvergeer.nl>
2018-08-13 17:12:01 +0200

    start para

--- js/index.js
+++ js/index.js
@@ -2,18 +2,21 @@ $(function(){
   var seuss = $("#seuss");
   var christmasTree = $('#christmas');
 
-    $(window).bind("scroll", function() {
-      var $this = $(this);
-      if ($this.scrollTop() >= 300) {
-        seuss.removeClass("fix-height");
-      } else {
-        seuss.addClass("fix-height");
-      }
+  $(window).bind("scroll", function() {
+    var $this = $(this);
+    if ($this.scrollTop() >= 300) {
+      seuss.removeClass("fix-height");
+    } else {
+      seuss.addClass("fix-height");
+    }
+  });
+});
 
-      if ($this.scrollTop() >= 700) {
-        $('#slide3').animate({
-          scrollTop: $('#slide3')[0].scrollHeight
-        }, 1000);
-      }
-    });
+$(function(){
+  $(window).bind("scroll", function() {
+    var $this = document.getElementById('slide4');
+    if ($this.scrollTop() >= 300) {
+      document.getElementById('#slide4').style.color = "red";
+    }
+  });
 });
