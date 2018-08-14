2deccfd8abcbf5b25d5ba41e514803d473e6931a
Jake <j.eggen@cevesvergeer.nl>
2018-08-14 10:31:46 +0200

    changing background

--- js/index.js
+++ js/index.js
@@ -13,10 +13,87 @@ $(function(){
 });
 
 $(function(){
+  var slide4 = $('#slide4')
   $(window).bind("scroll", function() {
-    var $this = document.getElementById('slide4');
-    if ($this.scrollTop() >= 300) {
-      document.getElementById('#slide4').style.color = "red";
+    var $this = $(this);
+
+    // var $this = document.getElementById('slide4');
+    if ($this.scrollTop() >= 2000) {
+      slide4.addClass("light-background");
+    }
+  });
+});
+
+$(function(){
+  var slide4 = $('#slide4')
+  $(window).bind("scroll", function() {
+    var $this = $(this);
+
+    // var $this = document.getElementById('slide4');
+    if ($this.scrollTop() >= 3000) {
+      slide4.addClass("darken-background");
+    }
+  });
+});
+
+$(function(){
+  var slide4 = $('#slide4')
+  $(window).bind("scroll", function() {
+    var $this = $(this);
+
+    // var $this = document.getElementById('slide4');
+    if ($this.scrollTop() <= 3000) {
+      slide4.removeClass("darken-background");
+    }
+  });
+});
+
+$(function(){
+  var slide4 = $('#slide4')
+  $(window).bind("scroll", function() {
+    var $this = $(this);
+
+    // var $this = document.getElementById('slide4');
+    if ($this.scrollTop() >= 3500) {
+      slide4.removeClass("darken-background");
+      slide4.addClass("dark-background");
+    }
+  });
+});
+
+$(function(){
+  var slide4 = $("#slide4")
+  $(window).bind("scroll", function() {
+    var $this = $(this);
+
+    // var $this = document.getElementById("slide4");
+    if ($this.scrollTop() <= 3500) {
+      slide4.removeClass("dark-background");
+    }
+  });
+});
+
+$(function(){
+  var slide4 = $('#slide4')
+  $(window).bind("scroll", function() {
+    var $this = $(this);
+
+    // var $this = document.getElementById('slide4');
+    if ($this.scrollTop() <= 4000) {
+      slide4.removeClass("darkest-background");
+    }
+  });
+});
+
+$(function(){
+  var slide4 = $('#slide4')
+  $(window).bind("scroll", function() {
+    var $this = $(this);
+
+    // var $this = document.getElementById('slide4');
+    if ($this.scrollTop() >= 4000) {
+      slide4.removeClass("dark-background");
+      slide4.addClass("darkest-background");
     }
   });
 });
